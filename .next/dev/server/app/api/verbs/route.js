48c2c68bf190c07565c8be6c38f93f20fb8f28b9
Alexander Veliz <alexanderveliz@MacBook-Pro-2.local>
2026-03-02 16:55:32 -0300

    Remove node_modules and .next from tracking

--- .next/dev/server/app/api/verbs/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/verbs/route.js")
-R.c("server/chunks/[root-of-the-server]__d39a8925._.js")
-R.c("server/chunks/[root-of-the-server]__150b3703._.js")
-R.c("server/chunks/_next-internal_server_app_api_verbs_route_actions_206f1cf6.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/verbs/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/verbs/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WcBT4TJ67HwkCoj5nPv4JuvclMapEhPQecv0DckxmNE=\"\n}"
